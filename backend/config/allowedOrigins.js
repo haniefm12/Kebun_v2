22bb850c2b88d9304ad25f65f481c5d6ed57e3b7
Muhammad Hanif <43397260+haniefm12@users.noreply.github.com>
2024-06-20 03:05:37 +0700

    middleware

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,5 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  //put future web url here
+];
+module.exports = allowedOrigins;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
 const app = express();
 
 const PORT = process.env.PORT || 3500;
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
@@ -18,5 +29,6 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not found");
   }
 });
+
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-//ff
