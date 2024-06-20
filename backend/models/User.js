8f284153b930d46d5aa3b480866fd447f65e6cc3
Muhammad Hanif <43397260+haniefm12@users.noreply.github.com>
2024-06-21 06:45:19 +0700

    backend user API Controller

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
-// import { Schema } from "mongoose";
-
-import bcrypt from "bcrypt";
-
-import validator from "validator";
 
 // Defining the user schema
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -23,7 +18,7 @@ const userSchema = new Schema({
   },
   phoneNumber: {
     type: String,
-    required: true,
+    // required: true,
   },
   roles: [
     {
@@ -41,18 +36,5 @@ const userSchema = new Schema({
   },
 });
 // Hashing the password before saving the user
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
-// Validating the user input
-userSchema.path("username").validate((username) => {
-  return validator.isAlphanumeric(username);
-}, "Username should be alphanumeric");
-userSchema.path("phoneNumber").validate((phoneNumber) => {
-  return validator.isMobilePhone(phoneNumber, "any");
-}, "Invalid phone number");
 
 module.exports = mongoose.model("User", userSchema);
