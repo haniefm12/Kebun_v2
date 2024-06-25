bc6050f0a76ae78adb7c7386d1c9835f8afb8c8e
Muhammad Hanif <43397260+haniefm12@users.noreply.github.com>
2024-06-25 07:16:38 +0700

    frontend: muiswitch darkmode fix

--- frontend/src/Components/Switch/index.jsx
+++ frontend/src/Components/Switch/index.jsx
@@ -24,7 +24,7 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
     },
   },
   "& .MuiSwitch-thumb": {
-    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    backgroundColor: theme.palette.mode === "light" ? "#003892" : "#001e3c",
     width: 32,
     height: 32,
     "&::before": {
