82aae2bc50acd42b10e943184401777d648e6725
Muhammad Hanif <43397260+haniefm12@users.noreply.github.com>
2024-07-19 06:13:11 +0700

    frontend inventaris page

---
+++ frontend/src/Components/features/inventory/Inventory.js
@@ -0,0 +1,74 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectInventoryById } from "../../../app/api/inventorysApiSlice";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import { IconButton, TableCell, TableRow } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { selectGardenById } from "../../../app/api/gardensApiSlice";
+
+const Inventory = ({ inventoryId, serialNumber }) => {
+  const inventory = useSelector((state) =>
+    selectInventoryById(state, inventoryId)
+  );
+  console.log(inventoryId);
+  const navigate = useNavigate();
+  const gardenId = inventory ? inventory.garden : null; // Add a check for note
+  const garden = useSelector((state) => selectGardenById(state, gardenId));
+  const formatDateTime = (date) => {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const dateObj = new Date(date);
+    const month = monthNames[dateObj.getMonth()];
+    const day = dateObj.getDate();
+    const year = dateObj.getFullYear();
+    const hour = dateObj.getHours();
+    const minute = dateObj.getMinutes();
+    return `(${hour.toString().padStart(2, "0")}:${minute
+      .toString()
+      .padStart(2, "0")})  ${day} ${month} ${year} `;
+  };
+
+  if (inventory) {
+    const handleEdit = () => navigate(`/inventaris/${inventoryId}`);
+
+    return (
+      <TableRow>
+        <TableCell sx={{ maxWidth: 10 }}>{serialNumber}</TableCell>
+        <TableCell align="left">
+          {garden ? garden.name : "Unknown garden"}
+        </TableCell>
+        <TableCell align="left">{inventory.item}</TableCell>
+        <TableCell align="left">{inventory.itemType}</TableCell>
+        <TableCell align="center">{inventory.quantity}</TableCell>
+        <TableCell align="center">
+          {formatDateTime(inventory.createdAt)}
+        </TableCell>
+        <TableCell align="center">
+          {inventory.createdAt === inventory.updatedAt
+            ? "-"
+            : formatDateTime(inventory.updatedAt)}
+        </TableCell>
+        <TableCell align="center">
+          <IconButton>
+            <EditNoteIcon fontSize="small" onClick={handleEdit} />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    );
+  } else return null;
+};
+
+export default Inventory;
--- frontend/src/Pages/Inventaris.jsx
+++ frontend/src/Pages/Inventaris.jsx
@@ -1,125 +1,100 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { TableVirtuoso } from "react-virtuoso";
-
-const sample = [
-  ["Frozen yoghurt", 159, 6.0, 24, 4.0],
-  ["Ice cream sandwich", 237, 9.0, 37, 4.3],
-  ["Eclair", 262, 16.0, 24, 6.0],
-  ["Cupcake", 305, 3.7, 67, 4.3],
-  ["Gingerbread", 356, 16.0, 49, 3.9],
-];
-
-function createData(id, dessert, calories, fat, carbs, protein) {
-  return { id, dessert, calories, fat, carbs, protein };
-}
-
-const columns = [
-  {
-    width: 200,
-    label: "Dessert",
-    dataKey: "dessert",
-  },
-  {
-    width: 120,
-    label: "Calories\u00A0(g)",
-    dataKey: "calories",
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: "Fat\u00A0(g)",
-    dataKey: "fat",
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: "Carbs\u00A0(g)",
-    dataKey: "carbs",
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: "Protein\u00A0(g)",
-    dataKey: "protein",
-    numeric: true,
-  },
-];
-
-const rows = Array.from({ length: 200 }, (_, index) => {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  return createData(index, ...randomSelection);
-});
-
-const VirtuosoTableComponents = {
-  Scroller: React.forwardRef((props, ref) => (
-    <TableContainer component={Paper} {...props} ref={ref} />
-  )),
-  Table: (props) => (
-    <Table
-      {...props}
-      sx={{ borderCollapse: "separate", tableLayout: "fixed" }}
-    />
-  ),
-  TableHead: React.forwardRef((props, ref) => (
-    <TableHead {...props} ref={ref} />
-  )),
+import React from "react";
+import { useGetInventorysQuery } from "../app/api/inventorysApiSlice";
+import Inventory from "../Components/features/inventory/Inventory";
+import AddIcon from "@mui/icons-material/Add";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Container,
+  Fab,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
   TableRow,
-  TableBody: React.forwardRef((props, ref) => (
-    <TableBody {...props} ref={ref} />
-  )),
-};
+  Typography,
+} from "@mui/material";
+import { AddCircleOutline } from "@mui/icons-material";
 
-function fixedHeaderContent() {
-  return (
-    <TableRow>
-      {columns.map((column) => (
-        <TableCell
-          key={column.dataKey}
-          variant="head"
-          align={column.numeric || false ? "right" : "left"}
-          style={{ width: column.width }}
+const Inventaris = () => {
+  const navigate = useNavigate();
+  const {
+    data: inventorys,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetInventorysQuery("inventoryList", {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  let content;
+
+  if (isLoading) content = <Typography>Loading...</Typography>;
+
+  if (isError) {
+    content = (
+      <Box variant="h1" className="errmsg">
+        {error?.data?.message}
+      </Box>
+    );
+  }
+
+  if (isSuccess) {
+    const { ids } = inventorys;
+
+    const tableContent = ids?.length
+      ? ids.map((inventoryId, index) => (
+          <Inventory
+            key={inventoryId}
+            inventoryId={inventoryId}
+            serialNumber={index + 1}
+          />
+        ))
+      : null;
+
+    content = (
+      <Box pl={4} pb={2} pt={2} pr={2}>
+        <Typography pl={2} pb={1} variant="h4">
+          Inventaris
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 580 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell width={5}>No.</TableCell>
+                <TableCell align="left">Nama Kebun</TableCell>
+                <TableCell align="left">Nama Barang</TableCell>
+                <TableCell align="left">Tipe Barang</TableCell>
+                <TableCell align="left">Jumlah Barang</TableCell>
+                <TableCell align="left">Tanggal Barang Masuk</TableCell>
+                <TableCell align="left">Tanggal Diperbaharui</TableCell>
+                <TableCell align="center">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{tableContent}</TableBody>
+          </Table>
+        </TableContainer>
+        <Fab
+          color="primary"
+          size="large"
           sx={{
-            backgroundColor: "background.paper",
+            position: "absolute",
+            bottom: 35,
+            right: 35,
           }}
+          onClick={() => navigate("/inventaris/new")}
         >
-          {column.label}
-        </TableCell>
-      ))}
-    </TableRow>
-  );
-}
+          <AddIcon />
+        </Fab>
+      </Box>
+    );
+  }
 
-function rowContent(_index, row) {
-  return (
-    <React.Fragment>
-      {columns.map((column) => (
-        <TableCell
-          key={column.dataKey}
-          align={column.numeric || false ? "right" : "left"}
-        >
-          {row[column.dataKey]}
-        </TableCell>
-      ))}
-    </React.Fragment>
-  );
-}
-
-export default function ReactVirtualizedTable() {
-  return (
-    <Paper style={{ height: 400, width: "100%" }}>
-      <TableVirtuoso
-        data={rows}
-        components={VirtuosoTableComponents}
-        fixedHeaderContent={fixedHeaderContent}
-        itemContent={rowContent}
-      />
-    </Paper>
-  );
-}
+  return content;
+};
+export default Inventaris;
