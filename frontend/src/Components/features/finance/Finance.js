190b49a4399bc21c5ca5f38b6123536e5475f204
Muhammad Hanif <43397260+haniefm12@users.noreply.github.com>
2024-07-19 07:15:45 +0700

    front end keuangan page

---
+++ frontend/src/Components/features/finance/Finance.js
@@ -0,0 +1,78 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectFinanceById } from "../../../app/api/financesApiSlice";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import { IconButton, TableCell, TableRow } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { selectGardenById } from "../../../app/api/gardensApiSlice";
+
+const Finance = ({ financeId, serialNumber }) => {
+  const finance = useSelector((state) => selectFinanceById(state, financeId));
+  console.log(financeId);
+  const navigate = useNavigate();
+  const gardenId = finance ? finance.garden : null; // Add a check for note
+  const garden = useSelector((state) => selectGardenById(state, gardenId));
+  const formatDateTime = (date) => {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const dateObj = new Date(date);
+    const month = monthNames[dateObj.getMonth()];
+    const day = dateObj.getDate();
+    const year = dateObj.getFullYear();
+    const hour = dateObj.getHours();
+    const minute = dateObj.getMinutes();
+    return `${day} ${month} ${year} `;
+  };
+
+  if (finance) {
+    const handleEdit = () => navigate(`/inventaris/${financeId}`);
+
+    return (
+      <TableRow>
+        <TableCell sx={{ maxWidth: 10 }}>{serialNumber}</TableCell>
+
+        <TableCell align="left">{finance.supplier}</TableCell>
+        <TableCell align="left">{finance.item}</TableCell>
+        <TableCell align="left">{finance.itemType}</TableCell>
+        <TableCell align="center">{finance.quantity}</TableCell>
+        <TableCell align="right">
+          <span style={{ float: "left" }}>Rp.</span>
+          <span style={{ float: "right" }}>
+            {finance.unitPrice.toLocaleString("id-ID")},-
+          </span>
+        </TableCell>
+        <TableCell align="left">
+          {" "}
+          <span style={{ float: "left" }}>Rp.</span>
+          <span style={{ float: "right" }}>
+            {finance.totalCost.toLocaleString("id-ID")},-
+          </span>
+        </TableCell>
+        <TableCell align="left">
+          {garden ? garden.name : "Unknown garden"}
+        </TableCell>
+        <TableCell align="left">{formatDateTime(finance.createdAt)}</TableCell>
+        <TableCell align="center">
+          <IconButton>
+            <EditNoteIcon fontSize="small" onClick={handleEdit} />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    );
+  } else return null;
+};
+
+export default Finance;
--- frontend/src/Pages/Keuangan.jsx
+++ frontend/src/Pages/Keuangan.jsx
@@ -1,7 +1,109 @@
 import React from "react";
+import { useGetFinancesQuery } from "../app/api/financesApiSlice";
+import Finance from "../Components/features/finance/Finance";
+import AddIcon from "@mui/icons-material/Add";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Container,
+  Fab,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { AddCircleOutline } from "@mui/icons-material";
 
-const Keuangan = () => {
-  return <div>Keuangan</div>;
+const Inventaris = () => {
+  const navigate = useNavigate();
+  const {
+    data: finances,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetFinancesQuery("financeList", {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  let content;
+
+  if (isLoading) content = <Typography>Loading...</Typography>;
+
+  if (isError) {
+    content = (
+      <Box variant="h1" className="errmsg">
+        {error?.data?.message}
+      </Box>
+    );
+  }
+
+  if (isSuccess) {
+    const { ids } = finances;
+    const tableContent = ids?.map((financeId, index) => (
+      <Finance key={financeId} financeId={financeId} serialNumber={index + 1} />
+    ));
+
+    const totalExpenses = ids?.reduce((acc, financeId) => {
+      console.log("finance", finances.entities[financeId]);
+      console.log(
+        "totalCost",
+        finances.entities[financeId]?.finance?.totalCost
+      );
+      return acc + (finances.entities[financeId]?.totalCost || 0);
+    }, 0);
+    console.log("ids", ids);
+    console.log("finances.entities", finances.entities);
+
+    content = (
+      <Box pl={4} pb={2} pt={2} pr={2}>
+        <Typography pl={2} pb={0} variant="h4">
+          Keuangan
+        </Typography>
+        <Typography variant="h6" sx={{ mt: 0, textAlign: "right" }}>
+          Total Pengeluaran: Rp. {totalExpenses.toLocaleString("id-ID")},-
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 580 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell width={5}>No.</TableCell>
+                <TableCell align="left">Supplier</TableCell>
+                <TableCell align="left">Nama Barang</TableCell>
+                <TableCell align="left">Tipe Barang</TableCell>
+                <TableCell align="left">Jumlah Barang</TableCell>
+                <TableCell align="left">Harga Satuan</TableCell>
+                <TableCell align="left">Jumlah Harga</TableCell>
+                <TableCell align="left">Nama Kebun</TableCell>
+                <TableCell align="left">Tanggal</TableCell>
+                <TableCell align="center">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{tableContent}</TableBody>
+          </Table>
+        </TableContainer>
+        <Fab
+          color="primary"
+          size="large"
+          sx={{
+            position: "absolute",
+            bottom: 35,
+            right: 35,
+          }}
+          onClick={() => navigate("/inventaris/new")}
+        >
+          <AddIcon />
+        </Fab>
+      </Box>
+    );
+  }
+
+  return content;
 };
-
-export default Keuangan;
+export default Inventaris;
