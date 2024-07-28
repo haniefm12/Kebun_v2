38cded9a7aad6c6db82f2c8f6e6431c741fdf810
Muhammad Hanif <43397260+haniefm12@users.noreply.github.com>
2024-07-29 03:23:49 +0700

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,15 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:8080",
-  "http://kebunapp.online/",
-  "http://103.134.154.6/",
-  "kebunapp.online",
-  "kebunapp.online/",
-  "https://kebunapp.online/",
-  "www.kebunapp.online",
-  "http://103.134.154.6:80",
-  "http://localhost:80",
-  "http://localhost:3500",
+  "http://kebunapp.online",
 
   //put future web url here
 ];
