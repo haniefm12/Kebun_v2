bd687cc70181deb0633bab9e6b477da99a8447da
Muhammad Hanif <43397260+haniefm12@users.noreply.github.com>
2024-07-29 03:20:59 +0700

    Update corsOptions.js

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -2,11 +2,10 @@ const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      console.error(`CORS error: origin ${origin} not allowed`);
-      callback(new Error("Not allowed by CORS ", origin, errorMessage));
+      callback(new Error(origin));
     }
   },
   credentials: true,
